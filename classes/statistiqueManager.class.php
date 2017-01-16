fc1be7a249dbbf5201b4560f0d8f52fa8706a746
Nicolas Rault <Nicolas Rault>
2017-01-16 19:04:34 +0100

    maj statistiqueManager

--- classes/statistiqueManager.class.php
+++ classes/statistiqueManager.class.php
@@ -6,8 +6,24 @@ public class StatistiqueManager{
     $this->db=$db;
   }
 
-  public function addStatistique($val){
+  public function add($statistique){
     $sql="INSERT INTO statistique VALUES(:num, :pointCollecte)";
+    $req=$this->db->prepare($sql);
+    $req->bindValue(":num", $statistique->getStatistique(),PDO::PARAM_STR);
+    $req->bindValue(":pointCollecte", $statistique->getPoint(),PDO::PARAM_INT);
+    $req->execute();
+  }
+
+  public function getStatistiqueByPoint($num){
+    $tabObj = array();
+    $sql="SELECT * FROM statistique WHERE point_num = :num";
+    $req=$this->db->prepare($sql);
+    $req->bindValue(":num", $num,PDO::PARAM_INT);
+    $req->execute();
+    while($ligne=$req->fetch(PDO::FETCH_OBJ)){
+      $tabObj = new Statistique($ligne);
+    }
+    return $tabObj;
   }
 }
  ?>
