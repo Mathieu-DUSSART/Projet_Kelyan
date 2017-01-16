ec1c843525785d12b0b7fd3d2378f08e6c3d3558
Mathieu-DUSSART <mathieud17700@gmail.com>
2017-01-16 08:56:55 +0100

    Ajout de la modification d'un article

--- include/pages/article.inc.php
+++ include/pages/article.inc.php
@@ -4,29 +4,51 @@
 //Affiche les articles de la page Article
 foreach ($managerArticle->getAllArticle(2) as $article) {
     //Récupère la date de l'article au format 23/09/2016
-    $date = date_format(date_create($article->getDate()), 'd/m/Y H:i');
-    ?>
-    <article>
+    $date = date_format(date_create($article->getDate()), 'd/m/Y H:i');?>
+
         <?php
-        echo "<h1>" . $article->getTitre() . "</h1>";
-        echo "<p class=\"dateArticle\">" . $date . "</h1>";
-        echo "<p>" . $article->getTexte() . "</p>";
+        if(isset($_POST["modifierArticle"]) && $_POST["numArticleAModifier"]==$article->getNum()){?>
+            <div id="ajouterArticle">
+                <form method="POST" action="#">
+                    <label>Titre de l'article:</label>
+                    <input  type="text" name="titre" value="<?php echo $article->getTitre(); ?>" required>
+                    <br>
+                    <label>Texte:</label>
+                    <textarea name="texte" rows="8" required><?php echo $article->getTexte(); ?></textarea>
+                    <br>
+                    <input class="bouton" type="submit" value="Modifier l'article">
+                </form>
+            </div>
+        <?php
+        }else{?>
+            <article>
+                <?php
+                echo "<h1>" . $article->getTitre() . "</h1>";
+                echo "<p class=\"dateArticle\">" . $date . "</h1>";
+                echo "<p>" . $article->getTexte() . "</p>";
+                ?>
+            </article>
+        <?php
+        }
         ?>
-    </article>
+
+
 
     <?php
-    if(isset($_SESSION["login"])){?>
-        <div class="voletGestionArticle">
-            <form class="supprimerArticle" method="POST" action="#">
-                <input name="supprimerArticle" type="submit" value="X">
-                <input name="numArticleASupprimer" type="hidden" value="<?php echo $article->getNum(); ?>">
-            </form>
-            <form class="modifierArticle" method="POST" action="#">
-                <input name="modifierArticle" type="submit" value="M">
-                <input name="numArticleAModifier" type="hidden" value="<?php echo $article->getNum(); ?>">
-            </form>
-        </div>
+    if(isset($_SESSION["login"])){
+        if(!isset($_POST["modifierArticle"]) || (isset($_POST["modifierArticle"]) && $_POST["numArticleAModifier"]!=$article->getNum())){?>
+            <div class="voletGestionArticle">
+                <form class="supprimerArticle" method="POST" action="#">
+                    <input name="supprimerArticle" type="submit" value="X">
+                    <input name="numArticleASupprimer" type="hidden" value="<?php echo $article->getNum(); ?>">
+                </form>
+                <form class="modifierArticle" method="POST" action="#">
+                    <input name="modifierArticle" type="submit" value="M">
+                    <input name="numArticleAModifier" type="hidden" value="<?php echo $article->getNum(); ?>">
+                </form>
+            </div>
         <?php
+        }
     }
 }
 
