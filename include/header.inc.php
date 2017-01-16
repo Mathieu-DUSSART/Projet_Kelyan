fed9748f0b2d1482308776d1c951a14da2159d2d
antoineDu87 <antoine.aymard.87@gmail.com>
2017-01-16 16:42:15 +0100

    blabla

--- include/header.inc.php
+++ include/header.inc.php
@@ -1,3 +1,12 @@
 <img id="logo" src="image/logo.jpeg" alt="">
 <h1>Association Kélyan</h1>
 <a href="https://www.facebook.com/Association-K%C3%A9lyan-940734619334207/?fref=ts" ><img id="logoFB" src="image/reseaux_sociaux/logoFB.png" alt=""></a>
+
+<!-- Add jQuery library -->
+	<script type="text/javascript" src="fancyBox-master\lib\jquery-1.10.2.min.js"></script>
+
+	<!-- Add mousewheel plugin (this is optional) -->
+	<script type="text/javascript" src="fancyBox-master\lib\jquery.mousewheel.pack.js?v=3.1.3"></script>
+
+	<!-- Add fancyBox main JS and CSS files -->
+	<script type="text/javascript" src="fancyBox-master\source\jquery.fancybox.pack.js?v=2.1.5"></script>
