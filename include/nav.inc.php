7be02de20159a6bb0d38697bba3b193331e62fde
Nicolas Rault <Nicolas Rault>
2017-01-19 08:48:36 +0100

    page statistique cachée

--- include/nav.inc.php
+++ include/nav.inc.php
@@ -69,7 +69,13 @@ if(isset($_POST["supprimerPage"])){
             if($numPage==$page->getNum()){
                 echo "<li><a href=\"index.php?page=" . $page->getNum() . "\" class=\"active\"><p class=\"page" . $page->getNum()%5 . "\">" . $page->getNom() . "</p></a>";
             }else{
+              if($page->getNom()=="Statistique"){
+                if(isset($_SESSION["login"])){
+                  echo "<li><a href=\"index.php?page=" . $page->getNum() . "\"><p class=\"page" . $page->getNum()%5 . "\">" . $page->getNom() . "</p></a>";
+                }
+              }else{
                 echo "<li><a href=\"index.php?page=" . $page->getNum() . "\"><p class=\"page" . $page->getNum()%5 . "\">" . $page->getNom() . "</p></a>";
+              }
             }
         }
         if(isset($_SESSION["login"]) && $managerPage->estSupprimable($page->getNum())){
