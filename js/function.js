f86b824877430eb99aafa119289c2dc6cd810d27
Mathieu-DUSSART <mathieud17700@gmail.com>
2017-01-19 07:54:34 +0100

    changement CSS

--- js/function.js
+++ js/function.js
@@ -30,14 +30,30 @@ function clickBoutonAjoutLogoReseauxSociaux(){
   }
 }
 
-function changerCouleur(){
+function afficherMenuDeroulant(){
+    var div = document.getElementById("menuDeroulant");
+    if(div.style.display=="block"){
+        div.style.display="none";
+    }else{
+        div.style.display="block";
+    }
+}
+
+function clickBoutonAfficherMenuDeroulant(){
+    var bouton = document.getElementById("menu");
+     if(bouton != null){
+    bouton.addEventListener("click", afficherMenuDeroulant, false);
+  }
+}
+
+/*function changerCouleur(){
   var couleurBackGround = document.getElementById("couleurFontPage").value;
   var couleurFooter = document.getElementById("couleurFooter").value;
   var body = document.getElementTagName("body");
   var footer = document.getElementTagName("footer");
   footer.style.background-color=couleurFooter;
-  body.style.
-}
+  body.style.background-color=couleurBackGround;
+}*/
 
 function clickBoutonChangerCouleur(){
   var bouton = document.getElementById("changerCouleur");
@@ -67,8 +83,48 @@ $( function() {
 } );
 
 
+$(function(){
+    var sommeLargeur = 0;
+    var nbElem = $('nav ul li').length;
+    var largeurNav = $('nav').width();
+    var largeurBouton = $('#ongletAjoutPage').width() + $('#menu').width();
+    $("nav li").each(function(){
+        sommeLargeur = sommeLargeur + $(this).width();
+        //alert(sommeLargeur + " tet " + (largeurNav))
+        if(sommeLargeur > (largeurNav-largeurBouton) && $(this).attr("id")!="menu" && $(this).attr("id")!="ongletAjoutPage"){
+            //alert(sommeLargeur + " tet " + (largeurNav-largeurBouton));
+            //alert(sommeLargeur + " coucou " + $('nav').width());
+            //alert(sommeLargeur);
+            //alert(compteur);
+            test(this);
+        }else{
+            //alert(sommeLargeur);
+            //alert("oops");
+            //test(compteur);
+        }
+    });
+
+    var paddingGauche = largeurNav - (sommeLargeur-largeurBouton);
+    $("nav ul").css( "padding-left", paddingGauche/2 );
+});
+
+function test(onglet){
+    //var onglet = $('nav ul li').get(n);
+    $("#menuDeroulant").append(onglet);
+    /*onglet.style.position = "absolute";
+    onglet.style.bottom = "-8vh";
+    onglet.style.right = "0";*/
+    /*$("#menuDeroulant").append(onglet);
+    var onglet2 = $('nav ul li').get(2);
+    $("#menuDeroulant").append(onglet2);
+    var onglet3 = $('nav ul li').get(3);
+    $("#menuDeroulant").append(onglet3);*/
 }
+
+
 window.onload=function(){
+    //test();
+    clickBoutonAfficherMenuDeroulant();
     fancyboxQuiMarche();
     clickBoutonAjoutPage();
     clickBoutonAjoutLogoReseauxSociaux();
