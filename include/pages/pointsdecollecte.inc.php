08b93f2fe1f80169de4f75a49a896fdaab1dda39
arsène <arsenegrandon@gmail.com>
2017-02-16 14:14:03 +0100

    push obligatoire

--- include/pages/pointsdecollecte.inc.php
+++ include/pages/pointsdecollecte.inc.php
@@ -110,6 +110,33 @@ if(isset($_POST["selectVillePoint"])){
                     </form>
                 </td>
                 </div>
+                <?php
+                // On prépare l'adresse à rechercher
+                $address = "Palais de l'Elysée 55, rue du faubourg Saint-Honoré 75008 Paris";
+
+                // On prépare l'URL du géocodeur
+                $geocoder = "http://maps.googleapis.com/maps/api/geocode/xml?address=%s&sensor=false";
+
+                // Pour cette exemple, je vais considérer que ma chaîne n'est pas
+                // en UTF8, le géocoder ne fonctionnant qu'avec du texte en UTF8
+                $url_address = utf8_encode($address);
+   
+                // Penser a encoder votre adresse
+                $url_address = urlencode($url_address);
+
+                // On prépare notre requête
+                $query = sprintf($geocoder,$url_address);
+
+                // On interroge le serveur
+                $results = file_get_contents($query);
+
+                // On affiche le résultat
+                $movies = new SimpleXMLElement($results);
+
+                echo $movies->result[0]->geometry[0]->location[0]->lat;
+                echo '<br/>';
+                echo $movies->result[0]->geometry[0]->location[0]->lng;
+                ?>
             <?php
             }
         }
